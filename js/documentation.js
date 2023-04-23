ec7d4c01a25a21a7366b9982baf5840be62549fb
What-ever-this-is <yuchestart@gmail.com>
2023-04-23 18:39:41 -0500

    Added a lot of things to documentation

--- js/documentation.js
+++ js/documentation.js
@@ -1,3 +1,44 @@
+const documentation = {
+    categories:[],
+    documents:{}
+}
 function main(){
+    fetchDox(location.href.includes("?"))
+}
+async function fetchDox(page){
+    let response = await fetch(`${location.href.replace("/documentation.html","").replace(/\?.*/,"")}/documentation/doclists.json`);
+    let json = await response.json();
+    let list = json.documents;
+    documentation.categories = json.categories
+    for(var i=0; i<list.length; i++){
+        response = await fetch(`${location.href.replace("/documentation.html","").replace(/\?.*/,"")}/documentation/${list[i]}.json`)
+        json = await response.json();
+        documentation.documents[list[i]] = json;
+    }
     
+    $("homeloadwheel").id.hidden = true
+    if(!page)
+    loadHomeTOC()
+
+}
+function $(name,parent){
+    return parent?{
+        class:parent.getElementsByClassName(name),
+        tag:parent.getElementsByTagName(name)
+    }:{
+        class:document.getElementsByClassName(name),
+        tag:document.getElementsByTagName(name),
+        id:document.getElementById(name)
+    }
+}
+function loadHomeTOC(){
+    const TOC = $("TOC")
+    for(var i=0; i<documentation.categories.length; i++){
+        var topic = document.createElement("li")
+        var subtopics = document.createElement("ul")
+        var topiclink = document.createElement("a")
+        topiclink.href=`?topic=${documentation.categories[i]}`
+        topic.appendChild(topiclink)
+        topic.appendChild(subtopics)
+    }
 }
