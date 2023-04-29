5d6dc2f50f5e35cce6a84b6a7fa105caca7eaf37
What-ever-this-is <yuchestart@gmail.com>
2023-04-28 22:04:59 -0500

    I realize heavy optimization is neaded

--- page/js/app.js
+++ page/js/app.js
@@ -9,7 +9,7 @@ function $(name,parent){
         class:document.getElementsByClassName(name)
     }
 }
-var render,scene,camera,mymesh,mymesh2,mymaterial,packagedmesh,mylight,mylight2,mytexture;
+var render,scene,camera,mymesh,mymesh2,mymaterial,mymaterial2,packagedmesh,mylight,mylight2,mytexture;
 var keybinds = {
     KeyW:false,
     KeyA:false,
@@ -28,22 +28,32 @@ function main(){
     $("date").id.innerText = new Date()
     //Wow now that's a lot of code gone.
     render = new Renderer($("emotionalDamage").id)
+    $("emotionalDamage").id.onkeydown = function(e){
+        keybinds[e.code] = true;
+    }
+    $("emotionalDamage").id.onkeydown = function(e){
+        keybinds[e.code] = false;
+    }
     scene = new Scene(render)
     scene.bgcolor = CATS.Color("#74d4bf")
     mytexture = new Texture($("amogus").id)
     mymaterial = new TexturedMaterial(mytexture)
-    mylight = new PointLight([0,0,6],1,1,"#FF0000","#FF0000")
+    mymaterial2 = new SingleColorMaterial("#FF0000")
+    mylight = new PointLight([0,0,0],1,0.5,"#FF0000","#FF0000")
     mymesh = new Plane(1,mymaterial)
+    mymesh2 = new Cube(1,mymaterial2)
     mymesh.scale([1,1,1])
-    //mymesh.rotate([180,0,0])
-    scene.moveCamera([0,2,6])
+    mymesh.rotate([180,0,0])
+    mymesh2.translate([0,0,-10])
+    scene.moveCamera([0,2,0])
     scene.addLight(mylight);
     scene.addObject(mymesh)
+    scene.addObject(mymesh2)
     scene.setFOV(45)
     function cat(){
-        mymesh.rotate([180,0,0])
     scene.render()
-    //requestAnimationFrame(cat)
+
+    requestAnimationFrame(cat)
     }
     cat()
 }
